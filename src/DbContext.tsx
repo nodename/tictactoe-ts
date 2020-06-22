3eb21c09e5ad84fd7513ddd8ff648301a2030dea
Alan Shaw <alan@nodename.com>
2020-06-22 19:06:50 -0400

    Added a comment

--- src/DbContext.tsx
+++ src/DbContext.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useState } from 'react';
 import { default_db } from './db';
 
 const setDb = (() => { }) as any;
+// These are dummy parameters, which will be replaced by the DbProvider below:
 export const DbContext = createContext([default_db, setDb]);
 
 export const DbProvider = (props: any) => {
