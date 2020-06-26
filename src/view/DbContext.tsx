ce729d2e36d0265ea7e83d069b6f3acadf0fc53a
Alan Shaw <alan@nodename.com>
2020-06-25 23:00:08 -0400

    Added a comment

--- src/view/DbContext.tsx
+++ src/view/DbContext.tsx
@@ -13,6 +13,9 @@ export const DbContext = createContext<DbContextType>(dummyDbContextValue);
 export const DbProvider = (props: { children: any }) => {
     const [db, setDb] = useState(default_db);
     return (
+        // There's only one update operation permitted on the db,
+        // namely adding a turn,
+        // so that's what we pass down:
         <DbContext.Provider value={[db, updateWithTurn(setDb)]}>
             {props.children}
         </DbContext.Provider>
